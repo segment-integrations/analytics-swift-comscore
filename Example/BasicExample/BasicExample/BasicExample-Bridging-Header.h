0520c627537f0ddb08d05f7b742ebefae9cef21c
Alan Charles <acharles@twilio.com>
2022-11-04 14:51:57 -0600

    open source release

---
+++ Example/BasicExample/BasicExample/BasicExample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  BasicExample-Bridging-Header.h
+//
+//
+//  Created by Alan Charles on 6/27/22.
+//
+
+#import <ComScore/ComScore.h>
+
+#ifndef BasicExample-Bridging-Header_h
+#define BasicExample-Bridging-Header_h
+
+
+#endif /* BasicExample-Bridging-Header_h */
---
+++ Example/BasicExample/BasicExample/BasicExampleApp.swift
@@ -0,0 +1,29 @@
+//
+//  BasicExampleApp.swift
+//  BasicExample
+//
+//  Created by Brandon Sneed on 2/23/22.
+//
+
+import SwiftUI
+import Segment
+import SegmentComscore
+
+@main
+struct BasicExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+extension Analytics {
+    static var main: Analytics {
+        let analytics = Analytics(configuration: Configuration(writeKey: "7O86AjgXwDLHxwXapRDkSFvqRzLA5n1V")
+                    .flushAt(1)
+                    .trackApplicationLifecycleEvents(true))
+        analytics.add(plugin: ComscoreDestination())
+        return analytics
+    }
+}
